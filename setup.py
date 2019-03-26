d5a97cfd8c0cd6ec9f98b0bfbd1f0a284eaf569e
Oliver Breitwieser <oliver.breitwieser@kip.uni-heidelberg.de>
2019-03-26 09:44:22 +0100

    setup.py: include numpy include dirs explicitly
    Bump version to 1.7.1
    
    Change-Id: I299d3766fa05b04b76b1e68a64b6e0aa237541d9

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 
 from setuptools import setup
 from Cython.Build import cythonize
-# from Cython.Distutils import build_ex
 
 import os.path as osp
+import numpy as np
 
 versionfile = osp.join(osp.dirname(osp.abspath(__file__)), "sbs", "version.py")
 with open(versionfile) as f:
@@ -16,4 +16,5 @@ setup(
         packages=["sbs", "sbs/db"],
         ext_modules=cythonize("sbs/cutils.pyx"),
         zip_safe=True,
+        include_dirs=[np.get_include()],
     )
