d4f43966465dc164d1412e630df345fc9e7751ea
Oliver Breitwieser <oliver.breitwieser@kip.uni-heidelberg.de>
2020-12-23 15:40:03 +0100

    Add test verifying bug in parent is fixed
    Ensure c/13303 fixes bug when using sinusoidal neurons.
    
    Change-Id: Ibe7db7c2550fd9cfe86f0db700ae2b01d9efeba5

--- tests/test_sinus_poisson_generator.py
+++ tests/test_sinus_poisson_generator.py
@@ -87,6 +87,57 @@ class TestSinusoidalPoissonGenerator(unittest.TestCase):
 
         bm.selected_sampler_idx = range(bm.num_samplers)
 
+    def test_sample_network_sinusoidal_poisson_rate_curr_same_spikes(self):
+        """
+            Verify that the network can be created and ran even if all neurons
+            receive the same background stimulus.
+        """
+        np.random.seed(424242)
+
+        # Load calibration data in order to create network.
+        sampler_config = sbs.db.SamplerConfiguration.load(
+                "test-calibration-curr.json")
+
+        # We set the variation behaviour of the rates via the source
+        # configuration of the sampler configuration. If we do not set it
+        # specifically, the source configuration from the calibration file
+        # would be used. Since a calibration on an array of different rates is
+        # not sensible, we set it here. We specify the weights, rates and times
+        # of each poisson input of a sampler. Details about the correct syntax
+        # are provided in the documentation of this source configuration class.
+
+        sampler_config.source_config = self.get_source_config()
+        sampler_config.source_config.individual_spike_trains = False
+
+        # Choose the number of samplers in the network.
+        bm = sbs.network.ThoroughBM(num_samplers=5, sim_name=sim_name,
+                                    sampler_config=sampler_config)
+
+        # Choose weights (here random) and symmetrize them.
+        weights = np.random.randn(bm.num_samplers, bm.num_samplers)
+        weights = (weights + weights.T) / 2.
+        bm.weights_theo = weights
+
+        # Choose biases (here random).
+        bm.biases_theo = np.random.randn(bm.num_samplers)
+
+        # Sample the network and save it.
+        bm.gather_spikes(
+                duration=1e5,  burn_in_time=500., dt=0.1,
+                sim_setup_kwargs=sbs.utils.get_default_setup_kwargs(sim_name))
+
+        log.info("Weights (theo):\n" + pf(bm.weights_theo))
+        log.info("Biases (theo):\n" + pf(bm.biases_theo))
+
+        log.info("Weights (bio):\n" + pf(bm.weights_bio))
+        log.info("Biases (bio):\n" + pf(bm.biases_bio))
+
+        log.info("Spikes: {}".format(pf(bm.ordered_spikes)))
+
+        log.info("Spike-data: {}".format(pf(bm.spike_data)))
+
+        bm.selected_sampler_idx = range(bm.num_samplers)
+
     def test_sample_network_sinusoidal_poisson_rate_curr(self):
         """
             How to setup and evaluate a Boltzmann machine. Please note that in
